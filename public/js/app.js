83a6eb4c89ff26a201d43d74e2daedb839959c44
gepopp <office@sewemo.eu>
2019-03-31 22:29:21 +0200

    js debug socket

--- public/js/app.js
+++ public/js/app.js
@@ -59245,8 +59245,8 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   encrypted: true,
   wsHost: window.location.hostname,
   wsPort: 6001,
-  wssPort: 6001,
-  disableStats: true
+  disableStats: true,
+  wssPort: 6001
 });
 
 /***/ }),
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -55,6 +55,6 @@ window.Echo = new Echo({
     encrypted: true,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssPort: 6001,
-    disableStats: true
+    disableStats: true,
+    wssPort: 6001
 });
