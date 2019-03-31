c9329f1f0278f63dbb507cdbf46d1c9613f5ff43
gepopp <office@sewemo.eu>
2019-03-31 23:00:42 +0200

    revert ssl

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -54,6 +54,5 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssPort: 6001,
     disableStats: true,
 });
