ce1c0cab2df0221228a3aecc9b37a7b47721e56f
gepopp <office@sewemo.eu>
2019-03-31 22:05:45 +0200

    sll suport for socket

--- public/js/app.js
+++ public/js/app.js
@@ -59242,9 +59242,10 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
   key: "asdf",
   cluster: "mt1",
-  //  encrypted: true,
+  encrypted: true,
   wsHost: window.location.hostname,
   wsPort: 6001,
+  wssPort: 6001,
   disableStats: true
 });
 
